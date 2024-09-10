cbd0996480390de07cebbd1f1507aa130ed00392
Guilherme Pimenta <guilhermepimenta976@gmail.com>
2024-09-10 19:25:22 -0300

    controller update

--- src/main/java/com/senai/apimuralvagas/controllers/EmpresaController.java
+++ src/main/java/com/senai/apimuralvagas/controllers/EmpresaController.java
@@ -3,22 +3,41 @@ package com.senai.apimuralvagas.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.senai.apimuralvagas.models.EmpresaModel;
 import com.senai.apimuralvagas.services.EmpresaService;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("empresa")
 public class EmpresaController {
 	@Autowired
 	private EmpresaService empresaService;
-	
+
 	@GetMapping
-	public List<EmpresaModel> returnAll(){
-		return empresaService.returnEmpresa();
+	public List<EmpresaModel> returnAllEmpresasC() {
+		return empresaService.returnAllEmpresas();
+	}
+
+/* 	@PostMapping
+	public ResponseEntity<EmpresaModel> criarEmpresa(@Valid @RequestBody EmpresaModel empresa) {
+		EmpresaModel novaEmpresa = empresaService.postEmpresa(empresa);
+		return new ResponseEntity<>(novaEmpresa, HttpStatus.CREATED);
+	} */
+
+	@PostMapping
+	public ResponseEntity<EmpresaModel> criarEmpresa(@RequestBody EmpresaModel empresa) {
+		System.out.println("Dados recebidos: " + empresa);
+		EmpresaModel empresaSalva = empresaService.postEmpresa(empresa);
+		return new ResponseEntity<>(empresaSalva, HttpStatus.CREATED);
 	}
 	
 
