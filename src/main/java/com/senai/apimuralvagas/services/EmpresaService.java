ccd0a98724ec867aa0b5c6f2b4f9f5923192381a
Guilherme Pimenta <guilhermepimenta976@gmail.com>
2024-09-10 19:24:08 -0300

    service update

--- src/main/java/com/senai/apimuralvagas/services/EmpresaService.java
+++ src/main/java/com/senai/apimuralvagas/services/EmpresaService.java
@@ -4,21 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.senai.apimuralvagas.models.EmpresaModel;
 import com.senai.apimuralvagas.repositorys.EmpresaRepo;
+
 
 @Service
 public class EmpresaService {
 	@Autowired
 	private EmpresaRepo empresaRepo;
 	
-	public  List<EmpresaModel> returnEmpresa(){
+	public  List<EmpresaModel> returnAllEmpresas(){
 		if(empresaRepo != null) {
 			return empresaRepo.findAll();
 		}
 		else {
 			return null;
 		}
+		
+	}
+	@Transactional
+	public EmpresaModel postEmpresa(EmpresaModel empresaModel) {
+		return empresaRepo.save(empresaModel);
 	}
 }
