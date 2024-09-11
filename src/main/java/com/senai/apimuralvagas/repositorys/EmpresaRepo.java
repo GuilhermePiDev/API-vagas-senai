9ade5c5af41efca8a410d456b574634fb2ea8301
GuilhermePiDev <guilhermepimenta@gmail.com>
2024-09-10 21:20:27 -0300

    annotation @Repository

--- src/main/java/com/senai/apimuralvagas/repositorys/EmpresaRepo.java
+++ src/main/java/com/senai/apimuralvagas/repositorys/EmpresaRepo.java
@@ -1,9 +1,11 @@
 package com.senai.apimuralvagas.repositorys;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.senai.apimuralvagas.models.EmpresaModel;
 
+@Repository
 public interface EmpresaRepo extends JpaRepository<EmpresaModel, Integer> {
 
 }
