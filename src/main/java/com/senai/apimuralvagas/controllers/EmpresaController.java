10e11f52173ce7e45f39334d1108bce576636144
GuilhermePiDev <guilhermepimenta@gmail.com>
2024-09-10 21:17:45 -0300

    corrigindo import requestbody

--- src/main/java/com/senai/apimuralvagas/controllers/EmpresaController.java
+++ src/main/java/com/senai/apimuralvagas/controllers/EmpresaController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.senai.apimuralvagas.models.EmpresaModel;
 import com.senai.apimuralvagas.services.EmpresaService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import jakarta.validation.Valid;
 
 @RestController
