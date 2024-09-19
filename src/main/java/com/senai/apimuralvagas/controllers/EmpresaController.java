d669eb8f7b39f0f657e28e0774764539b4804d4a
Guilherme Pimenta <guilhermepimenta976@gmail.com>
2024-09-18 23:49:50 -0300

    delete, get one e patch controller

--- src/main/java/com/senai/apimuralvagas/controllers/EmpresaController.java
+++ src/main/java/com/senai/apimuralvagas/controllers/EmpresaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,24 +40,31 @@ public class EmpresaController {
 	public ResponseEntity<java.lang.Object> returnOneEmpresa(@PathVariable int id) {
 		try {
 			 empresa = empresaService.returnOneEmpresa(id);
-			return new ResponseEntity<>(empresa, HttpStatus.OK); // Use 200 OK para retorno bem-sucedido
+			return new ResponseEntity<>(empresa, HttpStatus.OK); 
 		} catch (EntityNotFoundException e) {
-			return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND); // Ainda retorna uma String com a mensagem de erro
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND); 
 		}
 	}
 
 	@PostMapping
-	public ResponseEntity<EmpresaModel> criarEmpresa(@Valid @RequestBody EmpresaModel empresa) {
-		empresaService.postEmpresa(empresa);
-		return new ResponseEntity<>(empresa, HttpStatus.CREATED);
+	public ResponseEntity<EmpresaModel> criarEmpresa( @RequestBody EmpresaModel empresa) {
+
+		try {
+			empresaService.postEmpresa(empresa);
+			return new ResponseEntity<>(empresa, HttpStatus.CREATED);
+			
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
 	}
 	
 	@DeleteMapping("/{id}")
-	public ResponseEntity<java.lang.Object> deletarEmpresa(@PathVariable int id){
+	public ResponseEntity<?> deletarEmpresa(@PathVariable int id){
 		try {
 			empresa = empresaService.returnOneEmpresa(id);
 			empresaService.deleteEmpresa(id);
-			return new ResponseEntity<>(empresa , HttpStatus.OK);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		} catch (EntityNotFoundException e) {
 			return new ResponseEntity<>(e.getMessage() , HttpStatus.NOT_FOUND);
 		}
