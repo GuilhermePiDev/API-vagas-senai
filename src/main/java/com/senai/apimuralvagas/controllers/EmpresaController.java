35889e1e95cce396cefbd1f0d9f5d51e9fc45172
Guilherme Pimenta <guilhermepimenta976@gmail.com>
2024-09-20 17:28:52 -0300

    limpando o controller, removendos os try catch

--- src/main/java/com/senai/apimuralvagas/controllers/EmpresaController.java
+++ src/main/java/com/senai/apimuralvagas/controllers/EmpresaController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,64 +19,41 @@ import com.senai.apimuralvagas.services.EmpresaService;
 
 import org.springframework.web.bind.annotation.RequestBody;
 
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping("empresa")
 public class EmpresaController {
-	EmpresaModel empresa;
 
-	@Autowired
-	private EmpresaService empresaService;
+    @Autowired
+    private EmpresaService empresaService;
 
-	@GetMapping
-	public List<EmpresaModel> returnAllEmpresasC() {
-		return empresaService.returnAllEmpresas();
-	}
+    @GetMapping
+    public List<EmpresaModel> returnAllEmpresasC() {
+        return empresaService.returnAllEmpresas();
+    }
 
-	@GetMapping("/{id}") 
-	public ResponseEntity<java.lang.Object> returnOneEmpresa(@PathVariable int id) {
-		try {
-			 empresa = empresaService.returnOneEmpresa(id);
-			return new ResponseEntity<>(empresa, HttpStatus.OK); 
-		} catch (EntityNotFoundException e) {
-			return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND); 
-		}
-	}
+    @GetMapping("/{id}")
+    public ResponseEntity<EmpresaModel> returnOneEmpresa(@PathVariable int id) {
+        EmpresaModel empresa = empresaService.returnOneEmpresa(id);
+        return new ResponseEntity<>(empresa, HttpStatus.OK);
+    }
 
-	@PostMapping
-	public ResponseEntity<EmpresaModel> criarEmpresa( @RequestBody EmpresaModel empresa) {
+    @PostMapping
+    public ResponseEntity<EmpresaModel> criarEmpresa(@RequestBody EmpresaModel empresa) {
+        empresaService.postEmpresa(empresa);
+        return new ResponseEntity<>(empresa, HttpStatus.CREATED);
+    }
 
-		try {
-			empresaService.postEmpresa(empresa);
-			return new ResponseEntity<>(empresa, HttpStatus.CREATED);
-			
-		} catch (Exception e) {
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarEmpresa(@PathVariable int id) {
+        empresaService.deleteEmpresa(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
-	}
-	
-	@DeleteMapping("/{id}")
-	public ResponseEntity<?> deletarEmpresa(@PathVariable int id){
-		try {
-			empresa = empresaService.returnOneEmpresa(id);
-			empresaService.deleteEmpresa(id);
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		} catch (EntityNotFoundException e) {
-			return new ResponseEntity<>(e.getMessage() , HttpStatus.NOT_FOUND);
-		}
-	}
-
-	@PatchMapping("/{id}")
-    public ResponseEntity<java.lang.Object> patchEmpresa(@Valid @PathVariable int id, @RequestBody EmpresaModel empresaPatch) {
-	try {
-		empresa = empresaService.updateEmpresaParcial(empresaPatch, id);
-		return new ResponseEntity<>(empresa , HttpStatus.OK);
-		
-	} catch (EntityNotFoundException e) {
-        return new ResponseEntity<>(e.getMessage() , HttpStatus.NOT_FOUND);
-	}
+    @PatchMapping("/{id}")
+    public ResponseEntity<EmpresaModel> patchEmpresa(@PathVariable int id, @RequestBody EmpresaModel empresaPatch) {
+        EmpresaModel empresa = empresaService.updateEmpresaParcial(empresaPatch, id);
+        return new ResponseEntity<>(empresa, HttpStatus.OK);
     }
 }
