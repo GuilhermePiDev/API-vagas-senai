4d90806b44d3e8dca885b8b56d00f81ea366fabf
Guilherme Pimenta <guilhermepimenta976@gmail.com>
2024-09-23 22:49:52 -0300

    cpf validator

--- src/main/java/com/senai/apimuralvagas/services/AdminService.java
+++ src/main/java/com/senai/apimuralvagas/services/AdminService.java
@@ -15,14 +15,60 @@ public class AdminService {
     private AdminRepo adminRepo;
 
     public AdminModel postAdmin(AdminModel admin) {
-        Optional<AdminModel> adminExist = adminRepo.findByCpf(admin.getCpf());
-        if (adminExist.isPresent()) {
-            throw new EntityAlreadyExist("Usuario","Cpf",admin.getCpf());
-        } 
-        else {
-            return adminRepo.save(admin);
+        Optional<AdminModel> adminCpf = adminRepo.findByCpf(admin.getCpf());
+        Boolean validCpf = CpfValidator(admin.getCpf());
+
+        if (validCpf) {
+            if (adminCpf.isPresent()) {
+                throw new EntityAlreadyExist("Usuario","Cpf",admin.getCpf());
+            } 
+            else {
+                return adminRepo.save(admin);
+            }
         }
+        else{
+            throw new InvalidDataException("Cpf", admin.getCpf());
+        }
+        
 
     }
+
+    public static boolean CpfValidator(String cpf) {
+        cpf = cpf.replaceAll("\\D", ""); 
+        if (cpf.length() != 11 || cpf.matches("(\\d)\\1{10}")) {
+            return false;
+        }
+        
+        // Cálculo do 1º 
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            sum += (cpf.charAt(i) - '0') * (10 - i);
+        }
+        
+        int firstCheck = 11 - (sum % 11);
+        
+        if (firstCheck >= 10) {
+            firstCheck = 0;
+        }
+        
+        if (firstCheck != (cpf.charAt(9) - '0')) {
+            return false;
+        }
+
+        // Cálculo do 2º 
+        sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += (cpf.charAt(i) - '0') * (11 - i);
+        }
+        
+        int secondCheck = 11 - (sum % 11);
+        
+        if (secondCheck >= 10) {
+            secondCheck = 0;
+        }
+        
+        return secondCheck == (cpf.charAt(10) - '0');
+    }
+    
 
 }
