2429e2b565029cde8a557baa19efcab8364c9c09
Guilherme Pimenta <guilhermepimenta976@gmail.com>
2024-09-26 18:38:03 -0300

    Validation exception controller

--- src/main/java/com/senai/apimuralvagas/exceptions/ExceptionController.java
+++ src/main/java/com/senai/apimuralvagas/exceptions/ExceptionController.java
@@ -1,7 +1,12 @@
 package com.senai.apimuralvagas.exceptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,5 +31,20 @@ public class ExceptionController {
     @ExceptionHandler(InvalidDataException.class)
     public ResponseEntity<String> InvalidDataException(InvalidDataException e){
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+
+     @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        // Extrair o nome do campo e a mensagem de erro personalizada
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
