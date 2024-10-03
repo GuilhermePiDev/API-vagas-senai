fd2dfb53bd399b957d70c790a2abab13a750a896
Guilherme Pimenta <guilhermepimenta976@gmail.com>
2024-10-03 00:06:53 -0300

    cors

---
+++ src/main/java/com/senai/apimuralvagas/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.senai.apimuralvagas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite CORS em todos os endpoints
+                        .allowedOrigins("*") // Substitua com os domínios permitidos
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos HTTP permitidos
+                        .allowedHeaders("*") // Cabeçalhos permitidos
+                        .allowCredentials(true); // Se você quiser permitir cookies e autenticação
+            }
+        };
+    }
+}
+
