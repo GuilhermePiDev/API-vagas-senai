75c23f5dc19c1090393e0b29b481cdd1f1b28f69
Guilherme Pimenta <guilhermepimenta976@gmail.com>
2024-10-08 18:18:11 -0300

    auth dto response

--- src/main/java/com/senai/apimuralvagas/DTO/AuthRequiereDto.java
+++ src/main/java/com/senai/apimuralvagas/DTO/AuthRequiereDto.java
@@ -2,6 +2,6 @@ package com.senai.apimuralvagas.DTO;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record AuthDto(@NotBlank String email,@NotBlank String senha) {
+public record AuthRequiereDto(@NotBlank String email,@NotBlank String senha) {
     
 }
---
+++ src/main/java/com/senai/apimuralvagas/DTO/AuthResponseDTO.java
@@ -0,0 +1,5 @@
+package com.senai.apimuralvagas.DTO;
+
+public record AuthResponseDTO(String token) {
+    
+}
