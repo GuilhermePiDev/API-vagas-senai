e25e71c7c562045adf824aadfbc46ed9fde7a235
Guilherme Pimenta <guilhermepimenta976@gmail.com>
2024-10-08 21:08:40 -0300

    vaga controller

--- src/main/java/com/senai/apimuralvagas/controllers/VagaController.java
+++ src/main/java/com/senai/apimuralvagas/controllers/VagaController.java
@@ -1,5 +1,35 @@
 package com.senai.apimuralvagas.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.senai.apimuralvagas.models.VagaModel;
+import com.senai.apimuralvagas.services.VagaService;
+
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/vagas")
 public class VagaController {
-    
+    @Autowired
+    private VagaService vagaService;
+
+    @GetMapping
+    public List<VagaModel> returnAllVagas(){
+        return vagaService.returnAllVagas();
+    }
+
+    @PostMapping
+    public VagaModel postVaga(@Valid @RequestBody VagaModel vaga){
+        return vagaService.postVaga(vaga);
+    }
 }
