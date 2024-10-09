8a36b0a68c3c0eab9dda69a7e4a06634cecb1e60
Guilherme Pimenta <guilhermepimenta976@gmail.com>
2024-10-08 21:10:17 -0300

    vaga service

--- src/main/java/com/senai/apimuralvagas/services/VagaService.java
+++ src/main/java/com/senai/apimuralvagas/services/VagaService.java
@@ -1,5 +1,26 @@
 package com.senai.apimuralvagas.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.senai.apimuralvagas.models.VagaModel;
+import com.senai.apimuralvagas.repositorys.VagaRepo;
+
+@Service
 public class VagaService {
+
+    @Autowired
+    private VagaRepo vagaRepo;
+
+    public List<VagaModel> returnAllVagas(){
+        return vagaRepo.findAll();
+
+    }
+
+    public VagaModel postVaga(VagaModel vaga){
+        return vagaRepo.save(vaga);
+    }
     
 }
