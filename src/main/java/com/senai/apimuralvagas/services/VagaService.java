631451c3615837e6591528853843e43f83d30566
Guilherme Pimenta <guilhermepimenta976@gmail.com>
2024-10-30 17:31:22 -0300

    vaga services

--- src/main/java/com/senai/apimuralvagas/services/VagaService.java
+++ src/main/java/com/senai/apimuralvagas/services/VagaService.java
@@ -1,12 +1,17 @@
 package com.senai.apimuralvagas.services;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.senai.apimuralvagas.exceptions.CustomAccessException;
+import com.senai.apimuralvagas.exceptions.EntityNotFoundException;
 import com.senai.apimuralvagas.models.VagaModel;
 import com.senai.apimuralvagas.repositorys.VagaRepo;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class VagaService {
@@ -19,8 +24,60 @@ public class VagaService {
 
     }
 
+    public VagaModel returnOneVaga(Integer id){
+        existVaga(id);
+        return vagaRepo.findById(id).orElse(null);
+    }
+
+
+    @Transactional
     public VagaModel postVaga(VagaModel vaga){
         return vagaRepo.save(vaga);
     }
+
+    @Transactional
+    public VagaModel updateEmpresaParcial(VagaModel empresaPatch, int id) throws CustomAccessException {
+
+        existVaga(id);
+        VagaModel empresaExistente = vagaRepo.findById(id).orElse(null);
+
+        Field[] fields = VagaModel.class.getDeclaredFields();
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            try {
+            
+                if (field.getName().equals("empresaId")) {
+                    continue;
+                }
+                Object value = field.get(empresaPatch);
+                if (value != null) {
+                    field.set(empresaExistente, value);
+                }
+
+            } catch (IllegalAccessException e) {
+                throw new CustomAccessException();
+            }
+
+        }
+
+        return vagaRepo.save(empresaExistente);
+    }
+
+
+    
+    public void deleteVaga(int id) {
+        existVaga(id);
+        vagaRepo.deleteById(id);
+    }
+
+    private void existVaga(Integer id){
+        if (!vagaRepo.existsById(id)) {
+            throw new EntityNotFoundException("Vaga", id);
+        }
+    }
+
+
+
     
 }
