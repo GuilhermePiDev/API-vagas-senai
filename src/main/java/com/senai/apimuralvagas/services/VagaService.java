56098c51d744942be8927f6f9cd5b992eb47d289
Guilherme Pimenta <guilhermepimenta976@gmail.com>
2024-11-07 15:26:58 -0300

    Salvando na tabela vaga e empresaVaga

--- src/main/java/com/senai/apimuralvagas/services/VagaService.java
+++ src/main/java/com/senai/apimuralvagas/services/VagaService.java
@@ -4,11 +4,17 @@ import java.lang.reflect.Field;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import com.senai.apimuralvagas.exceptions.CustomAccessException;
 import com.senai.apimuralvagas.exceptions.EntityNotFoundException;
+import com.senai.apimuralvagas.models.EmpresaModel;
+import com.senai.apimuralvagas.models.EmpresaVagaModel;
 import com.senai.apimuralvagas.models.VagaModel;
+import com.senai.apimuralvagas.repositorys.EmpresaRepo;
+import com.senai.apimuralvagas.repositorys.EmpresaVagaRepo;
 import com.senai.apimuralvagas.repositorys.VagaRepo;
 
 import jakarta.transaction.Transactional;
@@ -19,20 +25,41 @@ public class VagaService {
     @Autowired
     private VagaRepo vagaRepo;
 
-    public List<VagaModel> returnAllVagas(){
+    @Autowired
+    private EmpresaRepo empresaRepo;
+
+    @Autowired
+    private EmpresaVagaRepo empresaVagaRepo;
+
+    public List<VagaModel> returnAllVagas() {
         return vagaRepo.findAll();
 
     }
 
-    public VagaModel returnOneVaga(Integer id){
+    public VagaModel returnOneVaga(Integer id) {
         existVaga(id);
         return vagaRepo.findById(id).orElse(null);
     }
 
-
     @Transactional
-    public VagaModel postVaga(VagaModel vaga){
-        return vagaRepo.save(vaga);
+    public VagaModel postVaga(VagaModel vaga) {
+      
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String email = userDetails.getUsername();
+
+      
+        EmpresaModel usuario = empresaRepo.findByEmail(email);
+
+        vaga = vagaRepo.save(vaga);
+
+        EmpresaVagaModel empresaVaga = new EmpresaVagaModel();
+        empresaVaga.setEmpresaId(usuario);
+        empresaVaga.setVagaId(vaga); 
+
+        
+        empresaVagaRepo.save(empresaVaga);
+
+        return vaga;
     }
 
     @Transactional
@@ -46,7 +73,7 @@ public class VagaService {
         for (Field field : fields) {
             field.setAccessible(true);
             try {
-            
+
                 if (field.getName().equals("empresaId")) {
                     continue;
                 }
@@ -64,20 +91,15 @@ public class VagaService {
         return vagaRepo.save(empresaExistente);
     }
 
-
-    
     public void deleteVaga(int id) {
         existVaga(id);
         vagaRepo.deleteById(id);
     }
 
-    private void existVaga(Integer id){
+    private void existVaga(Integer id) {
         if (!vagaRepo.existsById(id)) {
             throw new EntityNotFoundException("Vaga", id);
         }
     }
 
-
-
-    
 }
