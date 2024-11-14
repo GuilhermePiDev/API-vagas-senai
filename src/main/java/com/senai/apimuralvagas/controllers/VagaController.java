9a31c9e6a831e1b181c3dc343f7e377c1bb2ea86
GuilhermePiDev <guilhermepimenta@gmail.com>
2024-11-13 21:45:05 -0300

    apanhado geral

--- src/main/java/com/senai/apimuralvagas/controllers/VagaController.java
+++ src/main/java/com/senai/apimuralvagas/controllers/VagaController.java
@@ -2,12 +2,18 @@ package com.senai.apimuralvagas.controllers;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.senai.apimuralvagas.exceptions.CustomAccessException;
 import com.senai.apimuralvagas.models.VagaModel;
 import com.senai.apimuralvagas.services.VagaService;
 
@@ -26,7 +32,21 @@ public class VagaController {
     }
 
     @PostMapping
-    public VagaModel postVaga(@Valid @RequestBody VagaModel vaga){
-        return vagaService.postVaga(vaga);
+    public ResponseEntity<VagaModel> postVaga(@Valid @RequestBody VagaModel vaga){
+        VagaModel vagaModel = vagaService.postVaga(vaga);
+        return new ResponseEntity<>(vagaModel, HttpStatus.CREATED);
     }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<VagaModel> updateVaga(@PathVariable int id, @RequestBody VagaModel vaga) throws CustomAccessException{
+        VagaModel vagaModel = vagaService.updateVagaParcial(vaga, id);
+        return new ResponseEntity<>(vagaModel, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteVaga(@PathVariable int id) throws CustomAccessException{
+        vagaService.deleteVaga(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        
+    } 
 }
--- src/main/java/com/senai/apimuralvagas/exceptions/CustomAccessException.java
+++ src/main/java/com/senai/apimuralvagas/exceptions/CustomAccessException.java
@@ -3,7 +3,7 @@ package com.senai.apimuralvagas.exceptions;
 public class CustomAccessException extends IllegalAccessException{
 
     public CustomAccessException() {
-        super("Acesso negado para atualizar o(s) dado(s)");
+        super("Usuario sem permissão para executar essa ação");
     }
 
  
